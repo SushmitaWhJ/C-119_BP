a879c66eba2fbc81054d1ea583b2c8687c27e2e6
SushmitaWhJ <52954822+SushmitaWhJ@users.noreply.github.com>
2023-05-04 18:58:46 +0530

    Add files via upload

---
+++ data_preprocessing.py
@@ -0,0 +1,23 @@
+#Text Data Preprocessing Lib
+import nltk
+
+from nltk.stem import PorterStemmer
+stemmer = PorterStemmer()
+
+import json
+import pickle
+import numpy as np
+
+
+# function for appending stem words
+
+
+
+    
+        # Add all words of patterns to list
+        
+        # Add all tags to the classes list
+         
+
+#Create word corpus for chatbot
+
